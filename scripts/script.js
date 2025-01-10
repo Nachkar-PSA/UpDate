e1cd6537a5205a1c633242b6e77769602db761a0
PSA <nachkar.santyago@gmail.com>
2025-01-10 11:43:49 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll(".discover__link");
+const cards = document.querySelectorAll(".discover__cards");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    const cardDc = link.dataset.dl;
+    if (!cardDc) return;
+
+    cards.forEach((card) => {
+      card.classList.remove("active");
+    });
+
+    cards.forEach((card) => {
+      const dc = card.dataset.dc;
+      if (dc === cardDc) {
+        card.classList.add("active");
+      }
+    });
+
+    link.classList.add("active");
+  });
+});
